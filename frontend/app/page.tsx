d2a90a96310f5c44b37789185f1e847db3a34dd0
pushKowalski <atlasbob8@gmail.com>
2026-05-28 19:29:59 +0100

    feat: Frontend - fixed landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -14,8 +14,8 @@ export default function MarketingPage() {
               <Image
                 src="/logo.svg"
                 alt="Switch Logo"
-                width={32}
-                height={32}
+                width={100}
+                height={100}
                 priority
               />
             </div>
