2468fe3dfd83d86cc593a65e652971b4615a25df
pushKowalski <atlasbob8@gmail.com>
2026-06-02 10:59:29 +0100

    Testing cookie options for auth tokens 8

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -9,7 +9,7 @@ export const COOKIE_OPTIONS: CookieOptions = {
   sameSite: isProd ? 'none' : 'lax',
   maxAge: 30 * 24 * 60 * 60 * 1000,
   path: '/',
-  domain: isProd ? '.switchapp.space' : undefined,
+  domain: undefined,
 };
 
 export const RESEND_COOLDOWN_SECONDS = 60;
