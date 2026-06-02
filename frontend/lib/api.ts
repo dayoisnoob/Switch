cc100d45499715522b1c2247a9d89186423df504
pushKowalski <atlasbob8@gmail.com>
2026-06-02 12:47:16 +0100

    Testing cookie options for auth tokens 15

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -3,7 +3,7 @@ import { ApiError } from "./ApiError";
 import { toast } from "sonner";
 
 export const api = axios.create({
-  baseURL: "process.env.NEXT_PUBLIC_API_URL",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
 
